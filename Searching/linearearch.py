df2ce5bb5707c316928ab6eaabc4530eafd3198e
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-07-17 23:32:23 +0530

    Linear Search programe

---
+++ Searching/linearearch.py
@@ -0,0 +1,16 @@
+arr=[10,20,80,30,60,50,110,100]
+print('Enter number to be search')
+x=int(input())
+flag=False
+index=0
+for i in range(len(arr)):
+    if x== arr[i]:
+        flag=True
+        index=i
+        break
+    else:
+        flag=False
+if flag==True:
+    print('Number present at index',index)
+else:
+    print('Number Not Present')            
