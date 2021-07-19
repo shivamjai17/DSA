de6aa22ed3b4ceae845d14b4c3a71cc2c6e51408
shivam jaiswal <shivamjaiswal17032000@gmail.com>
2021-07-19 16:55:42 +0530

    file moved

--- Searching/Sorting/Bubble_Sort.py
+++
@@ -1,8 +0,0 @@
-#programe of bubble sort
-arr=[5,1,4,2,3]
-n=len(arr)
-for i in range(n):
-    for j in range(i,n-1):
-        if(arr[j+1]<arr[j]):
-            arr[j+1],arr[j]=arr[j],arr[j+1]
-print(arr)            
